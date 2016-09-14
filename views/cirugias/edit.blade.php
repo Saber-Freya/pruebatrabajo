cdc7a66642afbab6afd36ff6e88f670b7bbfc44f
Programacion3 <programacion3@e-consulting.com.mx>
2016-09-14 13:10:42 -0600

    Borrado
    Se borraron todas las carpetas

--- views/cirugias/edit.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('app')
-@section('content')
-@include('common.errors')
-<div class="container">
-    {!! Form::model($cirugia, ['route' => ['cirugias.update', $cirugia->id], 'method' => 'patch']) !!}
-        @include('cirugias.fields')
-    {!! Form::close() !!}
-</div>
-@endsection
